e1c10ab96451168affd9950b660657b83cefe2e5
Stéphan Zych <stephan@zych.be>
2025-02-13 23:01:17 +0100

    refactor: make StateController generic with AppStateSchema default

--- src/core/state-controller.ts
+++ src/core/state-controller.ts
@@ -1,21 +1,29 @@
 import { ReactiveController, ReactiveControllerHost } from 'lit';
 import { appState, AppStateSchema } from './state.js';
 
-export class StateController implements ReactiveController {
+interface StateManager<T> {
+  get<K extends keyof T>(key: K): T[K];
+  set<K extends keyof T>(key: K, value: T[K]): void;
+  subscribe(fn: () => void): () => void;
+}
+
+export class StateController<T = AppStateSchema> implements ReactiveController {
   private host: ReactiveControllerHost;
-  private keys: (keyof AppStateSchema)[];
+  private keys: (keyof T)[];
+  private state: StateManager<T>;
   private unsubscribe?: () => void;
   private prev: Record<string, unknown> = {};
 
-  constructor(host: ReactiveControllerHost, keys: (keyof AppStateSchema)[]) {
+  constructor(host: ReactiveControllerHost, keys: (keyof T)[], state?: StateManager<T>) {
     this.host = host;
     this.keys = keys;
+    this.state = state ?? (appState as unknown as StateManager<T>);
     host.addController(this);
   }
 
   hostConnected(): void {
     this.snapshot();
-    this.unsubscribe = appState.subscribe(() => {
+    this.unsubscribe = this.state.subscribe(() => {
       if (this.changed()) {
         this.snapshot();
         this.host.requestUpdate();
@@ -27,21 +35,21 @@ export class StateController implements ReactiveController {
     this.unsubscribe?.();
   }
 
-  get<K extends keyof AppStateSchema>(key: K): AppStateSchema[K] {
-    return appState.get(key);
+  get<K extends keyof T>(key: K): T[K] {
+    return this.state.get(key);
   }
 
-  set<K extends keyof AppStateSchema>(key: K, value: AppStateSchema[K]): void {
-    appState.set(key, value);
+  set<K extends keyof T>(key: K, value: T[K]): void {
+    this.state.set(key, value);
   }
 
   private snapshot(): void {
     for (const key of this.keys) {
-      this.prev[key] = appState.get(key);
+      this.prev[key as string] = this.state.get(key);
     }
   }
 
   private changed(): boolean {
-    return this.keys.some(key => appState.get(key) !== this.prev[key]);
+    return this.keys.some(key => this.state.get(key) !== this.prev[key as string]);
   }
 }
